ffcc03399aa6b01941fffa7e81c114d1172559ba
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-13 09:26:33 -0700

    Temp 12

--- 12/Two_Strings.c
+++ 12/Two_Strings.c
@@ -1,14 +1,8 @@
 #include<stdio.h>
 int main()
 {
-  int n1, n2;
-
-  printf("Enter length of first String.\n");
-  scanf("%d", &n1);n1++;
-  printf("Enter length of second String.\n");
-  scanf("%d", &n2);n2++;
-  
-  char s1[n1], s2[n2];
+  char s1[5];
+  char s2[5];
   
   printf("Enter the first String: ");
   gets(s1);
