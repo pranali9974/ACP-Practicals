eafd10a34f51524b6f289bcd45f0f3449722a8fd
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-15 01:21:45 +0530

    Finalized 5

--- 5/e.c
+++ 5/e.c
@@ -1,7 +1,9 @@
+//WAP to perform sort operation in 1-D array of size 10.
+
 /*
-	* In this algoriths, the array is sequentially scanned many times
-	* During each itteration, the pairs of consiquetive elements are compared
-	* The compared elements are interchanges if necessary to bring the in a specific order
+	* In this algoriths, the array is sequentially scanned many times.
+	* During each itteration, the pairs of consiquetive elements are compared.
+	* The compared elements are interchanges if necessary to bring the in the right order.
 	* O(n^2)
 	* Ω(n)
 	* Θ(n^2)
@@ -17,7 +19,6 @@ int main(void) {
 		scanf("%d", &arr[i]);
 
 	//Sorting
-
 	for(int i = 0;i < n;i++)
 		//This loop runnes the inner loop as many times as there are variables in the array
 		for(int j = 0;j < n;j++)
@@ -29,9 +30,7 @@ int main(void) {
 				arr[j] = arr[j] - arr[j+1];
 			}
 
-	printf("\n\n\n");
-
-	//Printing Array
+	printf("\n");
 
 	for(int i = 0;i < n;i++)
 		printf("%d\n", arr[i]);
@@ -42,4 +41,29 @@ int main(void) {
 	* int arr[] -->     the array to store the inserted values
 	* int i     -->     to itterate through outter loops
 	*int j      -->     to itterate through inner loops
+*/
+
+/*    Output
+	* Enter the 10 elements
+	> 1
+	> 6
+	> 8
+	> 7
+	> 2
+	> 5
+	> 4
+	> 3
+	> 9
+	> 10
+
+	* 1
+	* 2
+	* 3
+	* 4
+	* 5
+	* 6
+	* 7
+	* 8
+	* 9
+	* 10
 */
