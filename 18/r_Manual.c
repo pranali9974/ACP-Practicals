9d20223ea8092d9ac5ff3443ab75ac63161ab4f7
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-15 19:02:32 +0530

    Deubgged 18

--- 18/r_Manual.c
+++ 18/r_Manual.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	int n, m;
-	scanf("%d", &n);
-	scanf("%d", &m);
-	char first_string[n];
-	char second_string[m];
+	char first_string[5000], second_string[5000], third_string[10000];
 	int i;
 	printf("Enter the first String\n");
-	gets(first_string);
 	gets(first_string);
 	printf("Enter the second String\n");
 	gets(second_string);
 
-	for(i=0;first_string[i]!='\0';i++); 
+	strcpy(third_string, first_string);
+
+	for(i=0;first_string[i]!='\0';i++);
 
 	for(int j=0;second_string[j]!='\0';j++) {
 		first_string[i]=second_string[j];
