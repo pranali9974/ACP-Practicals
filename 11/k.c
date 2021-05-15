3615b51cb4f9476ea0889a23dd814e3d134b5cab
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-15 14:48:55 +0530

    Finalized 11

--- 11/k.c
+++ 11/k.c
@@ -1,8 +1,9 @@
+//WAP to add two matrix of size 2-D array.
+
 /*
- 	* Initially, the first array is accepted and also copied to the sum array.
+ 	* Initially, the first array is accepted and also copied to the third array.
 	* Then the second array is accepted.
-	* This second array data is also added to the sum array.
-	* After inserting is complete, we print the array for checking.
+	* This second array data is now added to the third array.
 */
 
 #include <stdio.h>
@@ -10,8 +11,7 @@
 int main(void) {
 	int arr[3][3], arr0[3][3], sum[3][3];
 
-	//Accepting first array and adding it to the sum array
-
+	//Accepting first array and adding it to the third array
 	printf("Enter the first 9 elements.\n");
 
 	for(int i = 0;i < 3;i++)
@@ -20,8 +20,7 @@ int main(void) {
 			sum[i][j] = arr[i][j];
 		}
 
-	//Accepting second array and adding it to the sum array
-
+	//Accepting second array and adding it to the third array
 	printf("Enter the second 9 elements.\n");
 
 	for(int i = 0;i < 3;i++)
@@ -30,7 +29,7 @@ int main(void) {
 			sum[i][j] += arr0[i][j];
 		}
 
-	printf("\n\n\n\nSum\n");
+	printf("\n");
 	for(int i = 0;i < 3;i++) {
 		for(int j = 0;j < 3;j++)
 			printf("%d\t", sum[i][j]);
@@ -40,10 +39,37 @@ int main(void) {
 
 
 
-/*
+/*    Variable List
  	* int i              -->		to itterate through the outter loop
 	* int j              -->		to itterate through the inner loop
 	* int a[][]          -->		to store data of the fitst array
 	* int b[][]          -->		to store data of the second array
 	* int sum[][]        -->		to store sum of the fitst and second array
+*/
+
+/*    Output
+	* Enter the first 9 elements.
+	> 1
+	> 2
+	> 3
+	> 4
+	> 5
+	> 6
+	> 7
+	> 8
+	> 9
+	* Enter the second 9 elements.
+	> 9
+	> 8
+	> 7
+	> 6
+	> 5
+	> 4
+	> 3
+	> 2
+	> 1
+
+	* 10      10      10
+	* 10      10      10
+	* 10      10      10
 */
