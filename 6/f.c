06e3b3e74471b9621e4b9559ee411ed488991d17
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-21 01:10:47 +0530

    Finalized 5

--- 6/f.c
+++ 6/f.c
@@ -1,12 +1,5 @@
 //WAP to merge two 1-D array in another 1-D array.
 
-/*
- 	* Initially, the first array is accepted.
-	* The first array is copied to the third array.
-	* The second array is accepted.
-	* The second array's data is appended to the third array.
-*/
-
 #include <stdio.h>
 
 int main(void) {
@@ -17,14 +10,12 @@ int main(void) {
 	scanf("%d", &m);
 	int a[n], b[m], mer[n+m];
 
-	//Accepting first array and coping it to third array
 	printf("Enter the first elements.\n");
 	for(int i = 0;i < n;i++) {
 		scanf("%d", &a[i]);
 		mer[i] = a[i];
 	}
 
-	//Accepting second array and appending it to the third array
 	printf("Enter the second elements.\n");
 	for(int i = 0;i < m;i++) {
 		scanf("%d", &b[i]);
@@ -35,15 +26,6 @@ int main(void) {
 	for(int i = 0;i < n+m;i++)
 		printf("%d\n", mer[i]);
 }
-
-/*    Variable List
-	* int n 			 	-->     to store the size of the first array
-	* int m 			 	-->     to store the size of the first array
-	* int a[]       	    -->		to store data of the fitst array
-	* int b[]               -->		to store data of the second array
-	* int mer[]  	        -->		to store first and second array
- 	* int i                 -->		to itterate through loops
-*/
 
 /*    Output
 	* Enter the size of the first arrar.
