91be24e7da3e60f23d85641ee3ab4b64f28057be
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-21 01:12:55 +0530

    Finalized 8

--- 8/h.c
+++ 8/h.c
@@ -1,11 +1,5 @@
 //WAP to read and print Two-D array of 3X3 elements.
 
-/*
-	* A 3*3, 2 dimentional array is declared.
-	* A nested loop is run 3*3 times to accept all 9 numbers of the array.
-	* a nested loop is run 3*3 times to print the 9 numbers in the array.
-*/
-
 #include <stdio.h>
 
 int main(void) {
@@ -13,12 +7,10 @@ int main(void) {
 
 	printf("Enter the %d elements.\n", n*n);
 
-	//Accepting the array
 	for(int i = 0;i < n;i++)
 		for(int j = 0;j < n;j++)
 			scanf("%d", &arr[i][j]);
 
-	//printing the array
 	printf("\n\n\n");
 	for(int i = 0;i < n;i++) {
 		for(int j = 0;j < n;j++)
@@ -26,13 +18,6 @@ int main(void) {
 		printf("\n");
 	}
 }
-
-/*    Variable List
-	* int n				   -->		to store the size of the array
-	* int arr[][]          -->		to store data of the fitst array
- 	* int i                -->		to itterate through the outter loop
- 	* int j                -->		to itterate through the inner loop
-*/
 
 /*    Output
 	* Enter the 9 elements.
