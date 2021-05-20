c037b63f737daa81c27a0d2ef9bfdc211fc6f1fd
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-05-21 01:13:36 +0530

    Finalized 810

--- 10/j.c
+++ 10/j.c
@@ -1,11 +1,5 @@
 //WAP to find maximum from given 2-D array.
 
-/*
-	* A 3*3, two dimentional array is declared.
-	* The array is itterated through to accept all numbers.
-	* The array is again itterated and the biggest number is recorded.
-*/
-
 #include <stdio.h>
 
 int main(void) {
@@ -13,14 +7,12 @@ int main(void) {
 
 	printf("Enter the 9 elements.\n");
 
-	//Accepting the array
 	for(int i = 0;i < 3;i++)
 		for(int j = 0;j < 3;j++)
 			scanf("%d", &arr[i][j]);
 
 	max = arr[0][0];
 
-	//Finding the largest number
 	for(int i = 0;i < 3;i++)
 		for(int j = 0;j < 3;j++)
 			if(max < arr[i][j])
@@ -28,14 +20,6 @@ int main(void) {
 
 	printf("\nMaximum = %d", max);
 }
-
-/*    Variable List
-	* int n				   -->		to store the size of the array
-	* int arr[][]          -->		to store data of the fitst array
-	* int max              -->      to store the largest number at any instance
- 	* int i                -->		to itterate through the outter loop
- 	* int j                -->		to itterate through the inner loop
-*/
 
 /*    Output
 	* Enter the 9 elements.
