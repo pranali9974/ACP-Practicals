2e5c1694921f29841c194b10fbf8ece9a9be5cb2
Dharmik_487 <64529217+Dharmik48@users.noreply.github.com>
2021-06-06 19:28:32 +0530

    Dharmik48: squashed bugs (#2)

--- 6/b.c
+++ 6/b.c
@@ -11,26 +11,26 @@ struct student {
 };
 
 int main(void) {
-	int tmp;
+	int tmp,i;
 	char t[5000], tt;
 	struct student s[5];
 
 	printf("Enter the 5 names.\n");
-	for(int i = 0;i < 5;i++){gets(t);strcpy(s[i].name, t);}
+	for(i = 0;i < 5;i++){gets(t);strcpy(s[i].name, t);}
 	printf("Enter the 5 rool numbers.\n");
-	for(int i = 0;i < 5;i++){scanf("%d", &tmp);s[i].rollno = tmp;}
+	for(i = 0;i < 5;i++){scanf("%d", &tmp);s[i].rollno = tmp;}
 	printf("Enter the genders of the 5 students.\nEnter M or F.\n");
-	for(int i = 0;i < 5;i++){scanf("%c", &tt);s[i].gender = tt;
-	printf("Enter for %d", i);}
+	for(i = 0;i < 5;i++){printf("Enter for %d", i+1);scanf(" %c", &tt);s[i].gender = tt;
+	}
 	// for(int i = 0;i < 5;i++){gets(t);strcpy(s[i].gender, t);}
 	// for(int i = 0;i < 5;i++){gets(t);strcpy(s[i].gender, t);}
 	printf("Enter the height of the 5 students.\n");
-	for(int i = 0;i < 5;i++){scanf("%d", &tmp);s[i].height = tmp;}
+	for(i = 0;i < 5;i++){scanf("%d", &tmp);s[i].height = tmp;}
 
-	for(int i = 0;i < 5;i++) {
+	for(i = 0;i < 5;i++) {
 		printf("Student name: %s\n", s[i].name);
 		printf("Student Roll no: %d\n", s[i].rollno);
-		printf("Student gender: %s\n", s[i].gender);
+		printf("Student gender: %c\n", s[i].gender);
 		printf("Student height: %d\n", s[i].height);
 		printf("\n\n");
 	}
